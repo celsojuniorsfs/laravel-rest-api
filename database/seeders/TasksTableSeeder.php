4a89ac3212b420181f02493f85a17e6aa1571740
Celso Junior <cjunior.sfs@gmail.com>
2022-03-14 01:55:38 -0300

    Seeder

---
+++ database/seeders/TasksTableSeeder.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class TasksTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        /*  factory(App\Models\Task::class)->create([
+            'name' => 'Estudar PHP',
+            'complete' => false
+        ]);
+
+        factory(App\Models\Task::class)->create([
+            'name' => 'Estudar JavaScript',
+            'complete' => true
+        ]);
+
+        factory(App\Models\Task::class)->create([
+            'name' => 'Estudar Laravel',
+            'complete' => false
+        ]);*/
+    }
+}
