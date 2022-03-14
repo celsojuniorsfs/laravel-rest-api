858f459371f79530fa1fa5e8f3310e5cacbc83ae
Celso Junior <cjunior.sfs@gmail.com>
2022-03-14 01:19:24 -0300

    store create Task

--- routes/api.php
+++ routes/api.php
@@ -1,27 +1,8 @@
 <?php
 
+use App\Http\Controllers\TasksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get("tasks", function () {
-    $tasks = [
-        [
-            "id" => 1,
-            "name" => "Estudar PHP 7",
-            "complete" => false
-        ],
-        [
-            "id" => 2,
-            "name" => "Estudar JavaScript",
-            "complete" => true
-        ]
-    ];
-
-    return $tasks;
-});
-
-/*
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-*/
+Route::get('tasks', [TasksController::class, 'index']);
+Route::post('tasks', [TasksController::class, 'store']);
